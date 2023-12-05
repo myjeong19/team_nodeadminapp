e9e3630f93e6e951693088f6abef57b9036f59ee
myjeong19@naver.com <myjeong19@naver.com>
2023-12-05 15:21:07 +0900

    feat. 라우터 설정

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/login', function (req, res, next) {
+  res.render('login');
+});
+
+router.get('/signUp', function (req, res, next) {
+  res.render('signUp');
+});
+
+router.get('/about', function (req, res, next) {
+  res.render('about');
 });
 
 module.exports = router;
