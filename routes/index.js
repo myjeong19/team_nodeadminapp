73d79e3b1c35483e45806eef0744002185552f6a
myjeong19@naver.com <myjeong19@naver.com>
2023-12-05 16:35:23 +0900

    feat. routes

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index');
-});
+router.get('/', (req, res, next) => res.render('index'));
 
-router.get('/login', function (req, res, next) {
-  res.render('login');
-});
+router.get('/login', (req, res, next) => res.render('login'));
 
-router.get('/about', function (req, res, next) {
-  res.render('about');
-});
+router.get('/about', (req, res, next) => res.render('about'));
 
-router.get('/signup', function (req, res, next) {
-  res.render('signup');
-});
+router.get('/signup', (req, res, next) => res.render('signup'));
 
 module.exports = router;
