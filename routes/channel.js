7eee9e68bed9e49ddf10f94bc9d347f9a850a8b5
myjeong19@naver.com <myjeong19@naver.com>
2023-12-13 00:44:38 +0900

    Fix. 라우터 수정

--- routes/channel.js
+++ routes/channel.js
@@ -4,62 +4,61 @@ var express = require('express');
 var router = express.Router();
 
 //localhost:3001/channel
-router.get('/', async(req,res)=>{
-    res.render('channel/list');
+router.get('/list', async (req, res) => {
+  res.render('channel/list');
 });
 
 //localhost:3001/channel/create
-router.get('/create', async(req,res)=>{
-    res.render('channel/create');
+router.get('/create', async (req, res) => {
+  res.render('channel/create');
 });
 
 //localhost:3001/channel/create
-router.post('/create', async(req,res)=>{
-    //채팅방 정보
-    //채팅방ID, 채팅방이름, 상태, 참여멤버ID, 참여유저상한수
-    //입력받는 정보: 채팅방이름 참여유저상한
-    var title=req.body.title;
-    var userLimit=req.body.userLimit;
+router.post('/create', async (req, res) => {
+  //채팅방 정보
+  //채팅방ID, 채팅방이름, 상태, 참여멤버ID, 참여유저상한수
+  //입력받는 정보: 채팅방이름 참여유저상한
+  var title = req.body.title;
+  var userLimit = req.body.userLimit;
 
-    //channel 객체로 전달
-    var channel={
-        title,
-        userLimit
-    };
+  //channel 객체로 전달
+  var channel = {
+    title,
+    userLimit,
+  };
 
-    res.redirect('/channel');
+  res.redirect('/channel');
 });
 
 //localhost:3001/channel/modify
-router.get('/modify', async(req,res)=>{
+router.get('/modify', async (req, res) => {
+  //임시로 전달할 예제 객체
+  var channel = {
+    title: '새채팅방',
+    channelId: 0,
+    status: '활동중',
+    userLimit: 100,
+  };
 
-    //임시로 전달할 예제 객체
-    var channel={
-        title:"새채팅방",
-        channelId:0,
-        status:"활동중",
-        userLimit:100
-    };
-
-    res.render('channel/modify', {channel});
+  res.render('channel/modify', { channel });
 });
 
 //localhost:3001/channel/modify
-router.post('/modify', async(req,res)=>{
-    var title=req.body.title;
-    var userLimit=req.body.userLimit;
+router.post('/modify', async (req, res) => {
+  var title = req.body.title;
+  var userLimit = req.body.userLimit;
 
-    //channel 객체로 전달
-    var channel={
-        title,
-        userLimit
-    };
+  //channel 객체로 전달
+  var channel = {
+    title,
+    userLimit,
+  };
 
-    res.redirect('/channel');
+  res.redirect('/channel');
 });
 
-router.get('/delete', async(req,res)=>{
-    res.render('channel/list');
+router.get('/delete', async (req, res) => {
+  res.render('channel/list');
 });
 
-module.exports=router;
+module.exports = router;
--- routes/message.js
+++ routes/message.js
@@ -1,147 +1,132 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/list', async (req, res) => {
+  var messages = [
+    {
+      channelNum: 1,
+      adminNum: 1,
+      nickName: '일돌이',
+      logging: 1,
+      chatId: 'one',
+      message: '밥먹자',
+      ipAddress: '111.111.111.111',
+      topNum: 1,
+      statusMessage: 1,
+      saveDate: Date.now(),
+      editDate: Date.now(),
+      deleteDate: Date.now(),
+    },
+    {
+      channelNum: 2,
+      adminNum: 2,
+      nickName: '이돌이',
+      logging: 2,
+      chatId: 'two',
+      message: '놀자',
+      ipAddress: '222.111.111.111',
+      topNum: 2,
+      statusMessage: 2,
+      saveDate: Date.now(),
+      editDate: Date.now(),
+      deleteDate: Date.now(),
+    },
+    {
+      channelNum: 3,
+      adminNum: 3,
+      nickName: '삼돌이',
+      logging: 3,
+      chatId: 'three',
+      message: '심심해',
+      ipAddress: '123.111.111.111',
+      topNum: 3,
+      statusMessage: 3,
+      saveDate: Date.now(),
+      editDate: Date.now(),
+      deleteDate: Date.now(),
+    },
+  ];
 
-
-
-router.get('/',async(req,res)=>{
-
-    var messages = [
-        {
-            channelNum:1,
-            adminNum:1,
-            nickName:'일돌이',
-            logging:1,
-            chatId:'one',
-            message:'밥먹자',
-            ipAddress:'111.111.111.111',
-            topNum:1,
-            statusMessage:1,
-            saveDate:Date.now(),
-            editDate:Date.now(),
-            deleteDate:Date.now()
-        },
-        {
-            channelNum:2,
-            adminNum:2,
-            nickName:'이돌이',
-            logging:2,
-            chatId:'two',
-            message:'놀자',
-            ipAddress:'222.111.111.111',
-            topNum:2,
-            statusMessage:2,
-            saveDate:Date.now(),
-            editDate:Date.now(),
-            deleteDate:Date.now()
-        },
-        {
-            channelNum:3,
-            adminNum:3,
-            nickName:'삼돌이',
-            logging:3,
-            chatId:'three',
-            message:'심심해',
-            ipAddress:'123.111.111.111',
-            topNum:3,
-            statusMessage:3,
-            saveDate:Date.now(),
-            editDate:Date.now(),
-            deleteDate:Date.now()
-        }
-    ];
-
-
-    res.render('message/list',{messages});
+  res.render('message/list', { messages });
 });
 
-
-router.get('/create',async(req,res)=>{
-    res.render('message/create');
+router.get('/create', async (req, res) => {
+  res.render('message/create');
 });
 
+router.post('/create', async (req, res) => {
+  var chatId = req.body.chatId;
+  var nickName = req.body.nickName;
+  var message = req.body.message;
 
-router.post('/create',async(req,res) => {
+  var messageDB = {
+    channelNum: 0,
+    adminNum: 0,
+    nickName,
+    logging: 0,
+    chatId,
+    message,
+    ipAddress: '123.111.111.111',
+    topNum: 0,
+    statusMessage: 0,
+    saveDate: Date.now(),
+    editDate: Date.now(),
+    deleteDate: Date.now(),
+  };
 
-    var chatId = req.body.chatId;
-    var nickName = req.body.nickName;
-    var message = req.body.message;
-
-    var messageDB = {
-        channelNum:0,
-        adminNum:0,
-        nickName,
-        logging:0,
-        chatId,
-        message,
-        ipAddress:'123.111.111.111',
-        topNum:0,
-        statusMessage:0,
-        saveDate:Date.now(),
-        editDate:Date.now(),
-        deleteDate:Date.now()
-    };
-
-    res.redirect('/message');
+  res.redirect('/message');
 });
 
+router.get('/modify/:aid', async (req, res) => {
+  var channelNum = req.params.aid;
 
-router.get('/modify/:aid', async (req,res) => {
+  var messageDB = {
+    channelNum,
+    adminNum: 1,
+    nickName: '일돌이',
+    logging: 1,
+    chatId: 'one',
+    message: '밥먹자',
+    ipAddress: '111.111.111.111',
+    topNum: 1,
+    statusMessage: 1,
+    saveDate: Date.now(),
+    editDate: Date.now(),
+    deleteDate: Date.now(),
+  };
 
-    var channelNum = req.params.aid;
-
-    var messageDB = {
-        channelNum,
-        adminNum:1,
-        nickName:'일돌이',
-        logging:1,
-        chatId:'one',
-        message:'밥먹자',
-        ipAddress:'111.111.111.111',
-        topNum:1,
-        statusMessage:1,
-        saveDate:Date.now(),
-        editDate:Date.now(),
-        deleteDate:Date.now()
-    };
-
-    res.render('message/modify',{messageDB});
+  res.render('message/modify', { messageDB });
 });
 
+router.post('/modify/:aid', async (req, res) => {
+  var channelNum = req.params.aid;
 
-router.post('/modify/:aid', async (req,res) => {
+  var chatId = req.body.chatId;
+  var nickName = req.body.nickName;
+  var message = req.body.message;
 
-    var channelNum = req.params.aid;
+  var messageDB = {
+    channelNum,
+    adminNum: 0,
+    nickName,
+    logging: 0,
+    chatId,
+    message,
+    ipAddress: '123.111.111.111',
+    topNum: 0,
+    statusMessage: 0,
+    saveDate: Date.now(),
+    editDate: Date.now(),
+    deleteDate: Date.now(),
+  };
 
-    var chatId = req.body.chatId;
-    var nickName = req.body.nickName;
-    var message = req.body.message;
-
-    var messageDB = {
-        channelNum,
-        adminNum:0,
-        nickName,
-        logging:0,
-        chatId,
-        message,
-        ipAddress:'123.111.111.111',
-        topNum:0,
-        statusMessage:0,
-        saveDate:Date.now(),
-        editDate:Date.now(),
-        deleteDate:Date.now()
-    };
-
-    res.redirect('/message');
+  res.redirect('/message');
 });
 
+router.get('/delete', async (req, res) => {
+  var channelNum = req.query.idx;
 
-router.get('/delete', async (req,res) => {
-
-    var channelNum =req.query.idx;
-
-    res.redirect('/message');
+  res.redirect('/message');
 });
-
 
 module.exports = router;
