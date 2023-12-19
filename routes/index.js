6b6f8a0fb6228a38b037e6082affaf5e0e4c9e01
dhdudtjr <youngsuk50@gmail.com>
2023-12-20 03:16:34 +0900

    feat: 관리자 웹사이트 뷰 구현  login/member

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function (req, res, next) {
-  res.render('index');
-});
 
-router.get('/login', async (req, res) =>
-  res.render('login', { title: 'Login' })
+
+
+/*
+-관리자 웹사이트의 로그인 웹페이지를 제공하는 라우팅 메소드
+-사용자 계정정보가 아닌 관리자 계정정보를 통한 로그인을 시도합니다
+-http://localhost:3001
+*/
+router.get('/', async (req, res) =>
+  res.render('login')
 );
 
-router.post('/login', async (req, res) => {
-  res.redirect('main');
+
+/*
+-관리자 계정으로 로그인 성공 이후에 최초로 보여줄 관리자 웹사이트 메인페이지
+-반드시 관리자 로그인 성공 후에 접속이 가능합니다
+-http://localhost:3001
+*/
+router.post('/', async (req, res) => {
+  res.redirect('/main');
 });
 
 router.get('/main', async (req, res) => res.render('main'));
