903f99e53fa451e9f3d1f07e759bcc5dcf172db1
dhdudtjr <youngsuk50@gmail.com>
2024-01-22 16:00:35 +0900

    feat: Redis 기반 분산 세션 환경 구성-young

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ require('dotenv').config();
 var sequelize = require('./models/index').sequelize;
 var session = require('express-session');
 
+//분산서버 기반 세션 관리를 위한 Redis 환경설정1
+const redis = require("redis");
+let RedisStore = require("connect-redis")(session);
+
+//세션 저장을 위한 레디스 서버 연결정보 설정하기
+let redisClient = redis.createClient({
+  host: "127.0.0.1",
+  port: 6379,
+  db: 0,
+  password: "test12345",
+});
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const adminRouter = require('./routes/admin');
@@ -19,17 +31,36 @@ const articleRouter = require('./routes/article');
 const app = express();
 sequelize.sync();
 
-app.use(session({
-  resave: false,
-  saveUninitialized: true, 
-  secret: "testsecret", 
-  cookie: {
-    httpOnly: true,
-    secure: false,
-    maxAge:1000 * 60 * 5 //5분동안 서버세션을 유지하겠다.(1000은 1초)
-  },
-}),
-);
+// //서버세션 설정
+// app.use(session({
+//   resave: false,
+//   saveUninitialized: true, 
+//   secret: "testsecret", 
+//   cookie: {
+//     httpOnly: true,
+//     secure: false,
+//     maxAge:1000 * 60 * 5 //5분동안 서버세션을 유지하겠다.(1000은 1초)
+//   },
+// }),
+// );
+
+
+app.use(
+  session({
+  store: new RedisStore({ client: redisClient }),
+    saveUninitialized: true,
+    secret: "testsecret",
+    resave: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      //maxAge: 3600000, //세션유지 시간설정 : 1시간
+    },
+      ttl : 250, //Redis DB에서 세션정보가 사라지게 할지에 대한 만료시간설정
+      token: "testsecret",
+    })
+  );
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
