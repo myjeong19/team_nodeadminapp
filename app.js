51c3ece94ebad51163015c78a591493f13b2d71a
ysungwon <hwoarang09@naver.com>
2024-01-22 14:56:11 +0900

    imsi

--- app.js
+++ app.js
@@ -1,44 +1,45 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const layout = require('express-ejs-layouts');
-var sequelize = require('./models/index').sequelize;
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const layout = require("express-ejs-layouts");
+var sequelize = require("./models/index").sequelize;
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const adminRouter = require('./routes/admin');
-const channelRouter = require('./routes/channel');
-const memberRouter = require('./routes/member');
-const messageRouter = require('./routes/message');
-const articleRouter = require('./routes/article');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const adminRouter = require("./routes/admin");
+const channelRouter = require("./routes/channel");
+const memberRouter = require("./routes/member");
+const messageRouter = require("./routes/message");
+const articleRouter = require("./routes/article");
 
 const app = express();
+
 sequelize.sync();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.set('layout', 'layout');
-app.set('layout extractScripts', true);
-app.set('layout extractStyles', true);
-app.set('layout extractMetas', true);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.set("layout", "layout");
+app.set("layout extractScripts", true);
+app.set("layout extractStyles", true);
+app.set("layout extractMetas", true);
 
 app.use(layout);
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/admin', adminRouter);
-app.use('/article', articleRouter);
-app.use('/channel', channelRouter);
-app.use('/member', memberRouter);
-app.use('/message', messageRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/admin", adminRouter);
+app.use("/article", articleRouter);
+app.use("/channel", channelRouter);
+app.use("/member", memberRouter);
+app.use("/message", messageRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -49,11 +50,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- models/index.js
+++ models/index.js
@@ -5,13 +5,11 @@ const Sequelize = require("sequelize");
 const env = process.env.NODE_ENV || "development";
 
 //DB연결 환경설정정보 변경처리//관련정보 수정
-const config = require(path.join(__dirname, "..", "config", "config.json"))[
-  env
-];
-
+//const config = require(path.join(__dirname, "..", "config", "config.js"))[env];
+const config = require("../config/config.js")[env];
 //데이터 베이스 객체
 const db = {};
-
+console.log("config : ", config);
 //DB연결정보로 시퀄라이즈 ORM 객체 생성
 const sequelize = new Sequelize(
   config.database,
@@ -19,7 +17,7 @@ const sequelize = new Sequelize(
   config.password,
   config
 );
-
+console.log("seq : ", sequelize);
 //DB 처리 객체에 시퀄라이즈 정보 맵핑처리
 //이후 DB객체를 통해 데이터 관리가능해짐
 db.sequelize = sequelize; //DB연결정보를 포함한 DB제어 객체속성(CRUD)
